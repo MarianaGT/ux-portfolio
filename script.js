4cd509f3a84f62eb41462126f9b2155704c4da1f
Mariana Galan <gatm870125@gmail.com>
2021-04-01 12:21:48 -0600

    onClick actions to display page and gif work

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
-// Get the modal
-var modal = document.getElementById("menuModal");
+// Get the page
+var page = document.getElementById("displayPage");
+// Get the gif
+var gif = document.getElementById("displayGif");
 
-// Get the button that opens the modal
-var btn = document.getElementById("menuBtn");
+// Get the button that opens the page
+var btn1 = document.getElementById("pageBtn");
+// Get the button that opens the gif
+var btn2 = document.getElementById("gifBtn");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
+// When the user clicks on the button, open the page
+btn1.onclick = function() {
+  page.style.display = "block";
+}
+// When the user clicks on the button, open the gif
+btn2.onclick = function() {
+  gif.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
-  modal.style.display = "none";
+  page.style.display = "none";
 }
