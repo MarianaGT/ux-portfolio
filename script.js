52031e15345fe8e68276ae76ce1644fbe4f0c6a1
Mariana Galan <gatm870125@gmail.com>
2021-04-01 13:06:53 -0600

    onClick actions to display chat works

--- script.js
+++ script.js
@@ -2,22 +2,28 @@
 var page = document.getElementById("displayPage");
 // Get the gif
 var gif = document.getElementById("displayGif");
+// Get the header content
+var content = document.getElementById("hideContent");
+// Get the header chat
+var chat = document.getElementById("displayChat");
+
 
 // Get the button that opens the page
-var btn1 = document.getElementById("pageBtn");
+var btn2 = document.getElementById("pageBtn");
 // Get the button that opens the gif
-var btn2 = document.getElementById("gifBtn");
+var btn1 = document.getElementById("gifBtn");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the page
-btn1.onclick = function() {
+btn2.onclick = function() {
   page.style.display = "block";
+  gif.style.display = "none";
+  chat.style.display = "block";
 }
 // When the user clicks on the button, open the gif
-btn2.onclick = function() {
+btn1.onclick = function() {
   gif.style.display = "block";
+  content.style.display = "none";
 }
 
 // When the user clicks on <span> (x), close the modal
