451eb85fb04fe21eeb235960c71a77f50fc8358e
Mariana Galan <gatm870125@gmail.com>
2021-05-12 15:31:39 -0500

    hamburguer menu icon added

--- script.js
+++ script.js
@@ -30,3 +30,22 @@ btn1.onclick = function() {
 span.onclick = function() {
   page.style.display = "none";
 }
+
+// Get the modal
+var modal = document.getElementById("menuModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("menuBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
